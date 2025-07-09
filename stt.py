fd1f26d5ee15344cd93232ee557083ba067da5e0
Ben Kahl <ben.kahl2002@gmail.com>
2025-07-08 22:16:37 -0700

    started speech to text'

--- stt.py
+++ stt.py
@@ -1,12 +1,45 @@
+from dotenv import load_dotenv
+import os
 import pvleopard
+from pvrecorder import PVRecorder
+import wave
+import struct
 
-leopard = pvleopard.create(access_key='${ACCESS_KEY}')
+
+load_dotenv()
+
+API_KEY = os.getenv('PICOVOICE_API_KEY')
+
+leopard = pvleopard.create(access_key=API_KEY)
+recorder = PVRecorder(device_index=-1, frame_length=512)
+
+
+def list_inputs():
+    for index, device in enumerate(PVRecorder.get_audio_devices()):
+        print(f'[{index}] {device}')
 
 
 def get_audio_data():
-    pass
+    audio = []
+    try:
+        recorder.start()
+        while True:
+            frame = recorder.read()
+            audio.extend(frame)
+        recorder.stop()
+        with wave.open('/user_input.wav', 'w') as f:
+            f.setparams((1, 2, 16000, 512, 'NONE', 'NONE'))
+            f.writeframes(struct.pack('h', * len(audio), *audio))
+        return audio
+    finally:
+        recorder.delete()
 
 
-def listen():
-    transcript, words = leopard.process(get_audio_data())
-    print(transcript)
+def speech_to_text():
+    input = get_audio_data()
+    if input:
+        transcript, words = leopard.process(input)
+        print(transcript)
+
+
+list_inputs()
