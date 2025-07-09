e8f746ad972b8921252837f09201a735ead52f34
Ben Kahl <ben.kahl2002@gmail.com>
2025-07-08 19:08:57 -0700

    bug fixes

--- fish_server.py
+++ fish_server.py
@@ -1,8 +1,29 @@
-from flask import Flask, request, render_template
+import atexit  # Import atexit for cleanup
+from flask import Flask, render_template, request
 from fish import Fish
 
 app = Flask(__name__)
-fish = Fish()
+fish_instance = None  # Initialize as None
+
+
+@app.before_request
+def initialize_fish():
+    # Initialize fish_instance only once
+    global fish_instance
+    if fish_instance is None:
+        fish_instance = Fish()
+        # Register the cleanup function to be called on app exit
+        atexit.register(cleanup_fish)
+
+
+def cleanup_fish():
+    # This function will be called when the application exits
+    print("Cleaning up GPIO pins...")
+    if fish_instance:
+        fish_instance.__head_motor.close()
+        fish_instance.__tail_motor.close()
+        fish_instance.__mouth_motor.close()
+        print("GPIO pins released.")
 
 
 @app.route('/')
@@ -11,27 +32,31 @@ def index():
 
 
 @app.route('/control_fish', methods=['POST'])
-def fish_control():
+def control_fish():
     action = request.form.get('action')
-    match action:
-        case 'move_head_out':
-            fish.move_head_out()
-            return 'Head moved out\n'
-        case 'move_head_in':
-            fish.move_head_in()
-            return 'Head moved in\n'
-        case 'move_tail_out':
-            fish.move_tail_out()
-            return 'Tail moved out\n'
-        case 'move_tail_in':
-            fish.move_tail_in()
-            return 'Tail moved in\n'
-        case 'move_mouth_out':
-            fish.move_mouth_out()
-            return 'Mouth moved out\n'
-        case 'move_mouth_in':
-            fish.move_mouth_in()
-            return 'Mouth moved in\n'
+    if fish_instance is None:
+        # Should not happen with before_request
+        return "Fish instance not initialized.", 500
+    if action == 'move_head_out':
+        fish_instance.move_head_out()
+        return "Head moved out!"
+    elif action == 'move_head_in':
+        fish_instance.move_head_in()
+        return "Head moved in!"
+    elif action == 'move_tail_out':
+        fish_instance.move_tail_out()
+        return "Tail moved out!"
+    elif action == 'move_tail_in':
+        fish_instance.move_tail_in()
+        return "Tail moved in!"
+    elif action == 'move_mouth_out':
+        fish_instance.move_mouth_out()
+        return "Mouth moved out!"
+    elif action == 'move_mouth_in':
+        fish_instance.move_mouth_in()
+        return "Mouth moved in!"
+    else:
+        return "Invalid action", 400
 
 
 if __name__ == '__main__':
