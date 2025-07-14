122dacef933de016c1b55c98210ac53a9eb91998
Ben Kahl <ben.kahl2002@gmail.com>
2025-07-14 15:38:03 -0700

    added wakeword detection and testing

--- stt.py
+++ stt.py
@@ -2,8 +2,7 @@ from dotenv import load_dotenv
 import os
 import pvleopard
 from pvrecorder import PvRecorder
-import wave
-import struct
+import pvporcupine
 import time
 
 
@@ -13,6 +12,8 @@ API_KEY = os.getenv('PICOVOICE_API_KEY')
 
 leopard = pvleopard.create(access_key=API_KEY)
 recorder = PvRecorder(device_index=-1, frame_length=512)
+# change keyword to custom model once it's created
+porcupine = pvporcupine.create(API_KEY, keywords=['bumblebee'])
 
 
 def list_inputs():
@@ -20,22 +21,43 @@ def list_inputs():
         print(f'[{index}] {device}')
 
 
+def get_next_audio_frame():
+    audio = []
+    try:
+        recorder.start()
+        frame = recorder.read()
+        audio.extend(frame)
+        recorder.stop()
+        return audio
+    except Exception as e:
+        print(f"Error getting audio frame {e}")
+
+
+def wakeword_detection():
+    audio_frame = get_next_audio_frame()
+    keyword_index = porcupine.process(audio_frame)
+    if keyword_index == 0:
+        print('wakeword detected, start speech to text')
+        speech_to_text()
+        return False
+    else:
+        return True
+
+
 def get_audio_data():
     audio = []
     start = time.time()
     duration = 5
     try:
+        print('starting audio recording')
         recorder.start()
         while time.time() - start < duration:
             frame = recorder.read()
             audio.extend(frame)
         recorder.stop()
-        # with wave.open('/user_input.wav', 'w') as f:
-        #   f.setparams((1, 2, 16000, 512, 'NONE', 'NONE'))
-        #  f.writeframes(struct.pack('h', * len(audio), *audio))
         return audio
-    finally:
-        recorder.delete()
+    except Exception as e:
+        print(f"Error getting audio data {e}")
 
 
 def speech_to_text():
@@ -43,10 +65,15 @@ def speech_to_text():
     if input:
         transcript, words = leopard.process(input)
         print(transcript)
+        return transcript
 
 
 if __name__ == "__main__":
     print('listing inputs...')
     list_inputs()
-    print('recording and transcribing')
-    speech_to_text()
+    detected = True
+    while detected:
+        detected = wakeword_detection()
+    recorder.delete()
+    porcupine.delete()
+    leopard.delete()
