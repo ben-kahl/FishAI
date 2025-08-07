f5c9fd810404da929504b3c637cd744dff85a75f
Ben Kahl <ben.kahl2002@gmail.com>
2025-08-07 11:30:42 -0700

    error fix in run_pipeline()

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     # Create web server and voice to response program
     flask_thread = threading.Thread(target=run_flask)
     picovoice_thread = threading.Thread(
-        target=run_pipeline, args=(shared_fish))
+        target=run_pipeline, args=(shared_fish,))
 
     # Start created threads
     flask_thread.start()
