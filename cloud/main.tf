5aa3f88f70384de37b91c3a7a3bbc8c1e9e3b352
Ben Kahl <ben.kahl2002@gmail.com>
2026-01-31 18:36:59 -0800

    updates to infra

--- cloud/main.tf
+++ cloud/main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "fishai-state"
+    key          = "dev/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+
 provider "aws" {
   region = "us-east-1"
 }
